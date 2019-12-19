9bdf16c6b26b7edda472539072ca80249e9ef9c1
sjkiss <sjkiss@gmail.com>
2019-12-19 11:48:02 -0500

    Updated install scripts

--- install_script.R
+++ install_script.R
@@ -1,15 +1,7 @@
-to.install<-c('usethis', 'learnr', 'devtools', 'tidyverse', 'magrittr', 'lubridate', 'haven', 'rmarkdown', 'datapasta', 'tibble', 'readr', 'foreign', 'dplyr', 'labelled', 'questionr', 'readxl',  'openxlsx', 'knitr', 'jsonlite', 'tidyr', 'DT', 'stringr', 'ggthemes', 'ggbeeswarm', 'glue', 'stringi', 'ggridges', 'viridis', 'scales', 'sf', 'tidycensus', 'tigris', 'censusapi', 'ggmap', 'leaflet', 'httpuv', 'cancensus', 'statcanR', 'cansim', 'valetr', 'weathercan', 'CANSIM2R', 'rcanvec', 'opendatatoronto', 'VancouvR', 'guardianapi', 'GuardianR', 'gapminder', 'broom', 'coefplot', 'cowplot', 'GGally', 'ggrepel', 'gridExtra', 'here', 'interplot', 'margins', 'maps', 'mapproj', 'mapdata', 'MASS', 'quantreg', 'rlang', 'survey', 'srvyr', 'viridisLite')
+to.install<-c('usethis', 'car', 'plotly', 'learnr', 'devtools', 'tidyverse', 'magrittr', 'lubridate', 'haven', 'rmarkdown', 'datapasta', 'tibble', 'readr', 'foreign', 'dplyr', 'labelled', 'questionr', 'readxl',  'openxlsx', 'knitr', 'jsonlite', 'tidyr', 'DT', 'stringr', 'ggthemes', 'ggbeeswarm', 'glue', 'stringi', 'ggridges', 'viridis', 'scales', 'sf', 'tidycensus', 'tigris', 'censusapi', 'ggmap', 'leaflet', 'httpuv', 'cancensus', 'statcanR', 'cansim', 'valetr', 'weathercan', 'CANSIM2R', 'rcanvec', 'opendatatoronto', 'VancouvR', 'guardianapi', 'GuardianR', 'gapminder', 'broom', 'coefplot', 'cowplot', 'GGally', 'ggrepel', 'gridExtra', 'here', 'interplot', 'margins', 'maps', 'mapproj', 'mapdata', 'MASS', 'quantreg', 'rlang', 'survey', 'srvyr', 'viridisLite')
 
 install.lib<-to.install[!to.install %in% installed.packages()]
 for(lib in install.lib) install.packages(lib,dependencies=TRUE)
-sapply(load.lib,require,character=TRUE)
-
-# UNCOMMENT AND RUN THE LINES BELOW IF YOU HAVE A MAC MACHINE
-
-install.packages("glue", type="mac.binary")
-install.packages("stringi", type="mac.binary")
-install.packages("stringr", type="mac.binary")
-install.packages("lubridate", type="mac.binary")
 
 #Install from github
 devtools::install_github("andrewbtran/muckrakr")
---
+++ install_script_with_plots.R
@@ -0,0 +1,23 @@
+to.install<-c('usethis', 'learnr', 'devtools', 'tidyverse', 'magrittr', 'lubridate', 'haven', 'rmarkdown', 'datapasta', 'tibble', 'readr', 'foreign', 'dplyr', 'labelled', 'questionr', 'readxl',  'openxlsx', 'knitr', 'jsonlite', 'tidyr', 'DT', 'stringr', 'ggthemes', 'ggbeeswarm', 'glue', 'stringi', 'ggridges', 'viridis', 'scales', 'sf', 'tidycensus', 'tigris', 'censusapi', 'ggmap', 'leaflet', 'httpuv', 'cancensus', 'statcanR', 'cansim', 'valetr', 'weathercan', 'CANSIM2R', 'rcanvec', 'opendatatoronto', 'VancouvR', 'guardianapi', 'GuardianR', 'gapminder', 'broom', 'coefplot', 'cowplot', 'GGally', 'ggrepel', 'gridExtra', 'here', 'interplot', 'margins', 'maps', 'mapproj', 'mapdata', 'MASS', 'quantreg', 'rlang', 'survey', 'srvyr', 'viridisLite')
+
+install.lib<-to.install[!to.install %in% installed.packages()]
+for(lib in install.lib) install.packages(lib,dependencies=TRUE)
+
+#Install from github
+devtools::install_github("andrewbtran/muckrakr")
+devtools::install_github("AliciaSchep/gglabeller") 
+devtools::install_github("kjhealy/socviz") 
+
+library(ggplot2)
+var1<-seq(1,100,1)
+var2<-seq(1,100,1)
+
+qplot(var1,var2, geom='point', main='if you get this plot, things are working. ')
+ggsave('test_plot_1.png')
+library(car)
+library(magrittr)
+var3<-sample(c('A', 'B', 'C'), 100, replace=T)
+var4<-Recode(var3, "'A'='Apple'; 'B'='Banana' ; 'C'='Cactus'")
+data.frame(var3, var4) %>% 
+  ggplot(., aes(x=var4))+geom_bar()+labs(title='IF you get this plot too things are working. ')
+ggsve('test_plot_2.png')
