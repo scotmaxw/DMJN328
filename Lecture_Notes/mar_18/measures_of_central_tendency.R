ed9a31c569b4015bffc0d67b84c58d254a61096a
sjkiss <sjkiss@gmail.com>
2020-03-18 09:58:17 -0400

    uploaded before class

--- Lecture_Notes/mar_18/measures_of_central_tendency.R
+++ Lecture_Notes/mar_18/measures_of_central_tendency.R
@@ -1,21 +1,37 @@
-## ----show-normal--------------------------------------------------------------
-#use rnorm to generate 10000 random numbers according to the normal distribution
-#mean of 1000 and standard deviation of 500
-var1<-rnorm(10000, mean=1000, sd=500)
-#make into a data frame
-df<-data.frame(var1)
-#graph a histogram
-ggplot(df, aes(x=var1))+geom_histogram()+theme_bw()+labs(title="Randomly Generated Normal Distribution")
-### Go back upand play with the mean and sd.
+## ----visualize-wages----------------------------------------------------------
+library(tidyverse)
+lfs %>%
+  ggplot(., aes(x=HRLYEARN))+geom_histogram()
 
+## ----mean-by-group------------------------------------------------------------
+lfs %>%
+ggplot(., aes(x=HRLYEARN))+geom_histogram()+facet_wrap(~as_factor(SEX))
+
+
+## ----show-means-by-sex--------------------------------------------------------
+lfs %>%
+group_by(SEX) %>%
+summarize(avg=mean(HRLYEARN, na.rm=T))
+
+## ----show-median-of-hourly-wages----------------------------------------------
+lfs %>%
+  group_by(SEX) %>%
+  summarize(median=median(HRLYEARN, na.rm=T))
+
+## ----graph-different-variances------------------------------------------------
+df %>%
+  #gather into Class and Grade
+  gather(Class, Grade) %>%
+  #Graph and facet
+  ggplot(., aes(x=Grade))+geom_histogram()+facet_wrap(~Class)
 
 ## ----show-uniform-------------------------------------------------------------
 #Sample a number from 1 to 6 (like on a die roll), 10000 times
 var1<-sample(1:6, 10000, replace=T)
 #turn into a dataframe (die)
 die<-data.frame(var1)
-#Graph as a 
-ggplot(df, aes(x=var1))+
+#Graph as a
+ggplot(die, aes(x=var1))+
   #as a barplot, counting the numbr of times each number occurs
   geom_bar(stat="count")+
   #turn it black and white
