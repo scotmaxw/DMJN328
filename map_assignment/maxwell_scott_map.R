3b01d61d94ec0d33d065846411e0aba2fd846344
scotmaxw <59572238+scotmaxw@users.noreply.github.com>
2020-04-22 11:30:36 -0400

    Delete 2016_pumf.sav

---
+++ map_assignment/maxwell_scott_map.R
@@ -0,0 +1,10 @@
+library(cancensus)
+library(tidyverse)
+options(cancensus.api_key='CensusMapper_0668eccd2a83200b3e8ab2810bba61b5')
+options(cancensus.cache_path = "~")
+
+
+st_marys<- get_census(dataset='CA16', regions=list(CSD='3531016'), vectors=c("v_CA16_2446"), labels="detailed", geo_format=NA, level="DA")
+
+library(ggplot2)
+ggplot(st_marys, aes(geometry=geometry, fill=Population))+geom_sf()
