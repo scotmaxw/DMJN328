7f0e217809e271d0b4c447b1f169d74cee1a4a9b
scotmaxw <59572238+scotmaxw@users.noreply.github.com>
2020-04-22 11:33:59 -0400

    Update maxwell_scott_map.R

--- map_assignment/maxwell_scott_map.R
+++ map_assignment/maxwell_scott_map.R
@@ -4,7 +4,9 @@ options(cancensus.api_key='CensusMapper_0668eccd2a83200b3e8ab2810bba61b5')
 options(cancensus.cache_path = "~")
 
 
-st_marys<- get_census(dataset='CA16', regions=list(CSD='3531016'), vectors=c("v_CA16_2446"), labels="detailed", geo_format=NA, level="DA")
+st_marys<- get_census(dataset='CA16', regions=list(CSD='3531016'),
+                      vectors=c("v_CA16_2446"), labels="detailed",
+                      geo_format="sf", level="DA")
 
 library(ggplot2)
 ggplot(st_marys, aes(geometry=geometry, fill=Population))+geom_sf()
